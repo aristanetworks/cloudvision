329bc6b6576d859588d7d8fc33d7839e554c3e52
Ayush Kumar <ayush.kumar@arista.com>
2021-12-15 14:37:14 -0800

    fix(timestamps): Add nanosecond support to timestamps
    Append nanoseconds value to seconds string
    Keep timestamp as a string value instead of parsing to int

--- packages/cloudvision-connector/src/Parser.ts
+++ packages/cloudvision-connector/src/Parser.ts
@@ -204,7 +204,10 @@ function decodeDeletes(deletes: string[]): CloudVisionDeletes<unknown> {
  * **NOTE** this is deprecated and in upcoming versions the timestamp will be passed on in its full
  * resolution. This behavior will become an option that can be turned on.
  */
-function convertNotif(notif: RawNotification): ConvertedNotification {
+function convertNotif(
+  notif: RawNotification,
+  nanosecondResolution?: boolean,
+): ConvertedNotification {
   const convertedNotif: ConvertedNotification = {
     path_elements: [],
     timestamp: 0,
@@ -216,7 +219,10 @@ function convertNotif(notif: RawNotification): ConvertedNotification {
     nanos += notif.timestamp.nanos;
   }
   nanos = nanos.padStart(9, '0');
-  convertedNotif.timestamp = parseInt(('' + notif.timestamp.seconds + nanos).slice(0, 13), 10);
+  const timestampAsString = '' + notif.timestamp.seconds + nanos;
+  convertedNotif.timestamp = nanosecondResolution
+    ? timestampAsString
+    : parseInt(timestampAsString.slice(0, 13), 10);
 
   if (notif.updates) {
     convertedNotif.updates = decodeNotifs(notif.updates);
@@ -237,8 +243,12 @@ function convertNotif(notif: RawNotification): ConvertedNotification {
  * @param batch the batched notifications to which these raw notifications will be added.
  * @param notif the raw notifications as they come from the API server.
  */
-function decodeAndBatchNotif(batch: CloudVisionBatchedNotifications, notif: RawNotification): void {
-  const convertedNotif: ConvertedNotification = convertNotif(notif);
+function decodeAndBatchNotif(
+  batch: CloudVisionBatchedNotifications,
+  notif: RawNotification,
+  nanosecondResolution: boolean,
+): void {
+  const convertedNotif: ConvertedNotification = convertNotif(notif, nanosecondResolution);
   if (batch.notifications[JSON.stringify(convertedNotif.path_elements)]) {
     batch.notifications[JSON.stringify(convertedNotif.path_elements)].push(convertedNotif);
   } else {
@@ -293,6 +303,7 @@ export function sortTimestamp(first: RawNotification, second: RawNotification): 
 export function decodeNotifications(
   result: CloudVisionRawNotifs,
   batchResults: boolean,
+  nanosecondResolution = false,
 ): CloudVisionBatchedNotifications | CloudVisionNotifs {
   result.notifications.sort(sortTimestamp);
 
@@ -305,7 +316,7 @@ export function decodeNotifications(
 
     for (let i = 0; i < result.notifications.length; i += 1) {
       const n = result.notifications[i];
-      decodeAndBatchNotif(batch, n);
+      decodeAndBatchNotif(batch, n, nanosecondResolution);
     }
     return batch;
   }
@@ -317,7 +328,7 @@ export function decodeNotifications(
   };
   for (let i = 0; i < result.notifications.length; i += 1) {
     const n = result.notifications[i];
-    const convertedNotif: ConvertedNotification = convertNotif(n);
+    const convertedNotif: ConvertedNotification = convertNotif(n, nanosecondResolution);
     notif.notifications.push(convertedNotif);
   }
   return notif;
@@ -392,12 +403,16 @@ export function encodeNotifications(
  * Implements parse which decodes responses sent by the server.
  */
 export default class Parser {
-  public static parse(data: string, batch: boolean): CloudVisionMessage {
+  public static parse(
+    data: string,
+    batch: boolean,
+    nanosecondResolution = false,
+  ): CloudVisionMessage {
     const message = JSON.parse(data);
     const result = message.result;
     if (result && !result.datasets && result.notifications) {
       // Timeseries notification that needs to be decoded with NEAT
-      const decodedResult = decodeNotifications(result, batch);
+      const decodedResult = decodeNotifications(result, batch, nanosecondResolution);
       return {
         error: message.error,
         result: decodedResult,
