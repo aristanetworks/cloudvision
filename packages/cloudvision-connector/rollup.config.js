174f8eff6be540c41a099c704ddc583dbdb18d6b
Albert Seo <seo@arista.com>
2022-06-24 12:19:28 -0700

    chore(deps): update eslint to v8.18.0
    also update eslint-plugin-import to v2.26.0

--- packages/cloudvision-connector/rollup.config.js
+++ packages/cloudvision-connector/rollup.config.js
@@ -1,5 +1,5 @@
 import commonjs from '@rollup/plugin-commonjs';
-import nodeResolve from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
